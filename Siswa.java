4b4b7b62646534cb0d65605ace583b3c1ac2059e
gopi <muhammad_raffi_33rpl@student.smktelkom-mlg.sch.id>
2024-08-19 14:14:08 +0700

    update tes

---
+++ Siswa.java
@@ -0,0 +1,29 @@
+package siswa;
+
+public class Siswa {
+    
+    //variable
+    int id;
+    String nama;
+    double ipk;
+
+
+    //Constructor
+    public Siswa() {
+        id = 0;
+        nama = "Kosong";
+        ipk = 0.0;
+    }
+
+    //Getter
+    //Setter
+    //method
+    public void print() {
+        System.out.println("ID : " + id );
+        System.out.println("Nama : " + id );
+        System.out.println(" IPK : " + id );
+    }        
+
+    
+    
+}
---
+++ SiswaTester.java
@@ -0,0 +1,48 @@
+//Driver Class
+package siswa;
+import java.lang.reflect.Constructor;
+
+public class SiswaTester {
+    public static void main(String[] args) {
+        //membuat Object
+        //namaClass namaObject = new
+        Siswa Raffi = new Siswa ();
+        Siswa Davin = new Siswa ();
+        Siswa Zaky = new Siswa ();
+        Siswa Dewangga = new Siswa ();
+        Siswa Rabbani = new Siswa ();
+
+        Raffi.id = 24;
+        Raffi.nama = "Raffi";
+        Raffi.ipk = 86.7;
+        Davin.id = 8;
+        Davin.nama = "Davin";
+        Davin.ipk = 85.2;
+        Zaky.id = 3;
+        Zaky.nama = "Zaky";
+        Zaky.ipk = 20.5;
+        Dewangga.id = 9;
+        Dewangga.nama = "Dewangga";
+        Dewangga.ipk = 5.1;
+        Rabbani.id = 23;
+        Rabbani.nama = "Rabbani";
+        Rabbani.ipk = 90.2;
+        Raffi.print();
+        System.out.println("-----------------");
+        Davin.print();
+        System.out.println("-----------------");
+        Zaky.print();
+        System.out.println("-----------------");
+        Dewangga.print();
+        System.out.println("-----------------");
+        Rabbani.print();
+        System.out.println("-----------------");
+    }
+
+
+
+
+}
+
+    
+
